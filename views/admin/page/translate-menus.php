ef72eaaaf6d198ac7f79b5849772a8a5d43a3328
Gilles Lengy <gilles.lengy@artaban.fr>
2022-11-23 16:32:33 +0100

    In the Babela pages, <!--             <li>                 <a href='<?php echo WEB_ROOT; ?>/admin/babela/list-exhibits-pages'>Expositions</a>             </li>             -->

--- views/admin/page/translate-menus.php
+++ views/admin/page/translate-menus.php
@@ -23,9 +23,11 @@ echo flash();
                 <a href='<?php echo WEB_ROOT; ?>/admin/babela/terms'>Termes</a>
             </li>
 
+            <!--
             <li>
                 <a href='<?php echo WEB_ROOT; ?>/admin/babela/list-exhibits-pages'>Expositions</a>
             </li>
+            -->
             <!--
             <li>
                 <a href='<?php echo WEB_ROOT; ?>/admin/babela/tags'>Tags</a>
